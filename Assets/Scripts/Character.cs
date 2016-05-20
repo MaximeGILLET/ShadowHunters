a7464510daf8da915efa55d81ea5de728dd6fa1e
maxime gillet <gilet666@hotmail.fr>
2016-05-20 20:58:04 +0200

    first

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Character :  NetworkBehaviour{
+
+	public string name;
+	public string type; //Shadow, hunter or neutral.
+	public string VictoryCondition;
+	public string Description;
+	public int HealPoints;
+	public int BaseDamage;
+	public string[] Quotes;
+	public Equipement[] Equiments;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
---
+++ Assets/Scripts/GlobalSystem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GlobalSystem : NetworkBehaviour {
+
+
+	public Character CurrentPlayer;
+	// Use this for initialization
+	NetworkClient myClient;
+
+	public void OnConnected(NetworkMessage netMsg) {
+		Debug.Log("Connected to server");
+	}
+
+	public void OnDisconnected(NetworkMessage netMsg) {
+		Debug.Log("Disconnected from server");
+	}
+
+	public void OnError(NetworkMessage netMsg) {
+		Debug.Log("Error connecting with code " + netMsg.reader.ReadString());
+	}
+
+
+	public void Start() {
+		myClient =new NetworkClient();
+	
+	
+		myClient.RegisterHandler(MsgType.Connect,OnConnected);
+		myClient.RegisterHandler(MsgType.Disconnect, OnDisconnected);
+		myClient.RegisterHandler(MsgType.Error, OnError);
+
+		myClient.Connect("127.0.0.1", 8888);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+      
+		// Character distribution
+
+
+		// Game routine 
+
+
+		// Game Ending (resolve winners)
+	}
+
+	private void ResolveVisionCards(){
+
+		// Resolve effects of vision cards if any.
+	}
+
+	private void PickLocation(){
+	
+		// Pick random location.
+
+		// Move current player token to location.
+	}
+
+	private void ApplyLocationEffect(){
+
+		// Apply effects (Draw cards, deal damage, steal equipement ...).
+	}
+
+	private void AttackPhase(){
+
+		// Chose a valid player to attack.
+
+		// Resolve Damages.
+
+	}
+}
